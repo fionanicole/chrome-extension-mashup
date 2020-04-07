a845b0cc62aaa5a41f1a54751ef7ec64cbdcfe49
fionanicole <fnh215@nyu.edu>
2020-04-07 14:09:10 -0400

    added a word redactor/highlighter

--- growgarden.js
+++ growgarden.js
@@ -21,3 +21,26 @@ function swapImg(img) {
   var newimg = chrome.extension.getURL("images/cornflower.jpg");
   img.src = newimg;
 }
+
+// Grab the entire document body
+// This gets an array even though it's likely just one thing
+var everything = document.getElementsByTagName("body");
+console.log('grow word redactor');
+
+for (var i = 0; i < everything.length; i++) {
+  // Look at the full content
+  var txt = everything[i].innerHTML;
+  //var s = "test the <img the> the";
+
+  // This is a way of splitting up by tags.
+  var tokens = txt.split(/(<.*?>)/);
+  for (var j = 0; j < tokens.length; j++) {
+    // Ignore anything that is a tag
+    if (tokens[j].charAt(0) !== '<') {
+      // Now replace the word "the" with "the" spanned with the class "redact"
+      tokens[j] = tokens[j].replace(/\bgrow\b/gi,'<span class="redact">grow</span>');
+    }
+  }
+  // Put everything back in
+  everything[i].innerHTML = tokens.join('');
+}
